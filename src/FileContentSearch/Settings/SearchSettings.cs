ddd255cdd4093a6522dbdc481d0f584e5b704806
DocBrown101 <me.home@outlook.de>
2020-12-29 20:05:28 +0100

    standard settings fixed

--- src/FileContentSearch/Settings/SearchSettings.cs
+++ src/FileContentSearch/Settings/SearchSettings.cs
@@ -1,7 +1,6 @@
 ﻿namespace FileContentSearch.Settings
 {
     using System;
-    using System.ComponentModel;
 
     [Serializable]
     public class SearchSettings
@@ -14,21 +13,22 @@
 
         public string ExcludedSubdirectoryName2 { get; set; }
 
-        [DefaultValue("Bitte auswählen ...")]
         public string SearchPath { get; set; }
 
-        [DefaultValue(".cs .xaml .tt")]
         public string FileExtensions { get; set; }
 
-        [DefaultValue(true)]
         public bool CheckUpperLowerCase { get; set; }
 
-        [DefaultValue(false)]
         public bool CheckAllFiles { get; set; }
 
-        [DefaultValue(false)]
         public bool ExcludSubdirectoryNames { get; set; }
 
         ////public List<LastFolderSettingModel> LastFolderList { get; set; }
+
+        public SearchSettings()
+        {
+            this.SearchPath = "Bitte auswählen ...";
+            this.FileExtensions = ".cs .xaml";
+        }
     }
 }
